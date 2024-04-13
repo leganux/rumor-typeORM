de4a6349e9cce14f588c53ed63cd516ba7861425
Angel Erick Cruz Olivera <leganux@hotmail.com>
2024-04-12 23:24:22 -0600

    :sparkles: Adding client RPGC connection to go

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -9,7 +9,9 @@ import (
 	"os"
 	"time"
 
-	productpb "api-gateway/generated/product"
+	eventpb "api-gateway/generated/event"
+	guestpb "api-gateway/generated/guest"
+	inviteRequestpb "api-gateway/generated/invite_request"
 	userpb "api-gateway/generated/user"
 	jwt "github.com/dgrijalva/jwt-go"
 	gcontext "github.com/gorilla/context"
@@ -20,10 +22,16 @@ import (
 
 // Client connections to gRPC services
 
-var productClient productpb.ProductServiceClient
 var userClient userpb.UserServiceClient
-var productServiceAddress = "product_service:50057"
-var userServiceAddress = "user_service:50058"
+var eventClient eventpb.EventServiceClient
+var guestClient guestpb.GuestServiceClient
+var inviteRequestClient inviteRequestpb.InviteRequestServiceClient
+
+var userServiceAddress = "user_service:50057"
+var eventServiceAddress = "event_service:50058"
+var guestServiceAddress = "guest_service:50059"
+var inviteRequestServiceAddress = "guest_service:50060"
+
 var rumorSecretKey = "rumor_secret_key"
 var rumorUser = "admin"
 var rumorPassword = "Kt3RickS0n"
@@ -36,19 +44,29 @@ type User struct {
 // Initialize gRPC clients
 func init() {
 
-	productConn, err := grpc.NewClient(productServiceAddress, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Failed to create client connection for product server: %v", err)
-	}
-	//defer productConn.Close()
-
-	productClient = productpb.NewProductServiceClient(productConn)
-
 	userConn, err := grpc.Dial(userServiceAddress, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to create client connection for user server: %v", err)
 	}
 	userClient = userpb.NewUserServiceClient(userConn)
+
+	inviteRequestConn, err := grpc.Dial(inviteRequestServiceAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to create client connection for user server: %v", err)
+	}
+	inviteRequestClient = inviteRequestpb.NewInviteRequestServiceClient(inviteRequestConn)
+
+	guestConn, err := grpc.Dial(guestServiceAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to create client connection for user server: %v", err)
+	}
+	guestClient = guestpb.NewGuestServiceClient(guestConn)
+
+	eventConn, err := grpc.Dial(eventServiceAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to create client connection for user server: %v", err)
+	}
+	eventClient = eventpb.NewEventServiceClient(eventConn)
 }
 
 func sendResponse(w http.ResponseWriter, status int, data interface{}, err error, message string) {
@@ -74,114 +92,7 @@ func sendResponse(w http.ResponseWriter, status int, data interface{}, err error
 	json.NewEncoder(w).Encode(response)
 }
 
-// Handlers for REST endpoints
-func createProductHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
-		return
-	}
-
-	var request productpb.CreateProductRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		sendResponse(w, http.StatusBadRequest, nil, err, "Invalid request body")
-		return
-	}
-
-	response, err := productClient.CreateProduct(context.Background(), &request)
-	if err != nil {
-		sendResponse(w, http.StatusInternalServerError, nil, err, "Error creating product")
-		return
-	}
-
-	sendResponse(w, http.StatusOK, response, nil, "OK")
-}
-func getProductByIdHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
-		return
-	}
-
-	id := r.URL.Query().Get("id")
-	if id == "" {
-		sendResponse(w, http.StatusBadRequest, nil, nil, "Missing product ID")
-		return
-	}
-
-	request := &productpb.FindProductByIdRequest{
-		Id: id,
-	}
-
-	response, err := productClient.FindProductById(context.Background(), request)
-	if err != nil {
-		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving product")
-		return
-	}
-
-	sendResponse(w, http.StatusOK, response, nil, "OK")
-}
-func updateProductHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPut {
-		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
-		return
-	}
-
-	var request productpb.UpdateProductRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		sendResponse(w, http.StatusBadRequest, nil, err, "Invalid request body")
-		return
-	}
-
-	response, err := productClient.UpdateProduct(context.Background(), &request)
-	if err != nil {
-		sendResponse(w, http.StatusInternalServerError, nil, err, "Error updating product")
-		return
-	}
-
-	sendResponse(w, http.StatusOK, response, nil, "OK")
-}
-func deleteProductHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodDelete {
-		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
-		return
-	}
-
-	id := r.URL.Query().Get("id")
-	if id == "" {
-		sendResponse(w, http.StatusBadRequest, nil, nil, "Missing product ID")
-		return
-	}
-
-	request := &productpb.DeleteProductRequest{
-		Id: id,
-	}
-
-	response, err := productClient.DeleteProduct(context.Background(), request)
-	if err != nil {
-		sendResponse(w, http.StatusInternalServerError, nil, err, "Error deleting product")
-		return
-	}
-
-	sendResponse(w, http.StatusOK, response, nil, "OK")
-}
-func getAllProductsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
-		return
-	}
-
-	request := &productpb.FindAllProductsRequest{}
-
-	response, err := productClient.FindAllProducts(context.Background(), request)
-	if err != nil {
-		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving products")
-		return
-	}
-
-	sendResponse(w, http.StatusOK, response, nil, "OK")
-}
-
 // user
-
 func createUserHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
@@ -202,7 +113,6 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	sendResponse(w, http.StatusOK, response, nil, "OK")
 }
-
 func getUserByIdHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
@@ -227,7 +137,6 @@ func getUserByIdHandler(w http.ResponseWriter, r *http.Request) {
 
 	sendResponse(w, http.StatusOK, response, nil, "OK")
 }
-
 func updateUserHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPut {
 		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
@@ -248,7 +157,6 @@ func updateUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	sendResponse(w, http.StatusOK, response, nil, "OK")
 }
-
 func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodDelete {
 		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
@@ -273,7 +181,6 @@ func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	sendResponse(w, http.StatusOK, response, nil, "OK")
 }
-
 func getAllUsersHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
@@ -285,6 +192,324 @@ func getAllUsersHandler(w http.ResponseWriter, r *http.Request) {
 	response, err := userClient.FindAllUsers(context.Background(), request)
 	if err != nil {
 		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving users")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+
+// event
+func createEventHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	var request eventpb.CreateEventRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		sendResponse(w, http.StatusBadRequest, nil, err, "Invalid request body")
+		return
+	}
+
+	response, err := eventClient.CreateEvent(context.Background(), &request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error creating event")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func getAllEventsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	request := &eventpb.FindAllEventsRequest{}
+
+	response, err := eventClient.FindAllEvents(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving events")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func getEventHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		sendResponse(w, http.StatusBadRequest, nil, nil, "Missing event ID")
+		return
+	}
+
+	request := &eventpb.FindEventByIdRequest{
+		Id: id,
+	}
+
+	response, err := eventClient.FindEventById(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving event")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func updateEventHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	var request eventpb.UpdateEventRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		sendResponse(w, http.StatusBadRequest, nil, err, "Invalid request body")
+		return
+	}
+
+	response, err := eventClient.UpdateEvent(context.Background(), &request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error updating event")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func deleteEventHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		sendResponse(w, http.StatusBadRequest, nil, nil, "Missing event ID")
+		return
+	}
+
+	request := &eventpb.DeleteEventRequest{
+		Id: id,
+	}
+
+	response, err := eventClient.DeleteEvent(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error deleting event")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+
+// guest
+func createGuestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	var request guestpb.CreateGuestRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		sendResponse(w, http.StatusBadRequest, nil, err, "Invalid request body")
+		return
+	}
+
+	response, err := guestClient.CreateGuest(context.Background(), &request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error creating guest")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func getAllGuestsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	request := &guestpb.FindAllGuestsRequest{}
+
+	response, err := guestClient.FindAllGuests(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving guests")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func getGuestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		sendResponse(w, http.StatusBadRequest, nil, nil, "Missing guest ID")
+		return
+	}
+
+	request := &guestpb.FindGuestByIdRequest{
+		Id: id,
+	}
+
+	response, err := guestClient.FindGuestById(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving guest")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func updateGuestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	var request guestpb.UpdateGuestRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		sendResponse(w, http.StatusBadRequest, nil, err, "Invalid request body")
+		return
+	}
+
+	response, err := guestClient.UpdateGuest(context.Background(), &request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error updating guest")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func deleteGuestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		sendResponse(w, http.StatusBadRequest, nil, nil, "Missing guest ID")
+		return
+	}
+
+	request := &guestpb.DeleteGuestRequest{
+		Id: id,
+	}
+
+	response, err := guestClient.DeleteGuest(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error deleting guest")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+
+// solicitudes de invite
+func createInviteRequestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	var request inviteRequestpb.CreateInviteRequestRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		sendResponse(w, http.StatusBadRequest, nil, err, "Invalid request body")
+		return
+	}
+
+	response, err := inviteRequestClient.CreateInviteRequest(context.Background(), &request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error creating invite request")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func getAllInviteRequestsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	request := &inviteRequestpb.FindAllInviteRequestsRequest{}
+
+	response, err := inviteRequestClient.FindAllInviteRequests(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving invite requests")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func getInviteRequestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		sendResponse(w, http.StatusBadRequest, nil, nil, "Missing invite request ID")
+		return
+	}
+
+	request := &inviteRequestpb.FindInviteRequestByIdRequest{
+		Id: id,
+	}
+
+	response, err := inviteRequestClient.FindInviteRequestById(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error retrieving invite request")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func updateInviteRequestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	var request inviteRequestpb.UpdateInviteRequestRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		sendResponse(w, http.StatusBadRequest, nil, err, "Invalid request body")
+		return
+	}
+
+	response, err := inviteRequestClient.UpdateInviteRequest(context.Background(), &request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error updating invite request")
+		return
+	}
+
+	sendResponse(w, http.StatusOK, response, nil, "OK")
+}
+func deleteInviteRequestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		sendResponse(w, http.StatusMethodNotAllowed, nil, nil, "Method not allowed")
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		sendResponse(w, http.StatusBadRequest, nil, nil, "Missing invite request ID")
+		return
+	}
+
+	request := &inviteRequestpb.DeleteInviteRequestRequest{
+		Id: id,
+	}
+
+	response, err := inviteRequestClient.DeleteInviteRequest(context.Background(), request)
+	if err != nil {
+		sendResponse(w, http.StatusInternalServerError, nil, err, "Error deleting invite request")
 		return
 	}
 
@@ -405,7 +630,6 @@ func main() {
 	password := os.Getenv("AUTH_PASSWORD")
 
 	if env == "develop" {
-		productServiceAddress = "localhost:50057"
 		userServiceAddress = "localhost:50057"
 	}
 	if secret_key != "" {
@@ -421,17 +645,32 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", loginHandler).Methods("POST")
 
-	router.HandleFunc("/api/products", getAllProductsHandler).Methods("GET")
-	router.HandleFunc("/api/products/create", createProductHandler).Methods("POST")
-	router.HandleFunc("/api/products/get", getProductByIdHandler).Methods("GET")
-	router.HandleFunc("/api/products/update", updateProductHandler).Methods("PUT")
-	router.HandleFunc("/api/products/delete", deleteProductHandler).Methods("DELETE")
-
 	router.HandleFunc("/api/users", getAllUsersHandler).Methods("GET")
 	router.HandleFunc("/api/users/create", createUserHandler).Methods("POST")
 	router.HandleFunc("/api/users/get", getUserByIdHandler).Methods("GET")
 	router.HandleFunc("/api/users/update", updateUserHandler).Methods("PUT")
 	router.HandleFunc("/api/users/delete", deleteUserHandler).Methods("DELETE")
+
+	//event
+	router.HandleFunc("/api/events", getAllEventsHandler).Methods("GET")
+	router.HandleFunc("/api/events/create", createEventHandler).Methods("POST")
+	router.HandleFunc("/api/events/get", getEventHandler).Methods("GET")
+	router.HandleFunc("/api/events/update", updateEventHandler).Methods("PUT")
+	router.HandleFunc("/api/events/delete", deleteEventHandler).Methods("DELETE")
+
+	// guest
+	router.HandleFunc("/api/guests", getAllGuestsHandler).Methods("GET")
+	router.HandleFunc("/api/guests/create", createGuestHandler).Methods("POST")
+	router.HandleFunc("/api/guests/get", getGuestHandler).Methods("GET")
+	router.HandleFunc("/api/guests/update", updateGuestHandler).Methods("PUT")
+	router.HandleFunc("/api/guests/delete", deleteGuestHandler).Methods("DELETE")
+
+	//invite request
+	router.HandleFunc("/api/invite-requests", getAllInviteRequestsHandler).Methods("GET")
+	router.HandleFunc("/api/invite-requests/create", createInviteRequestHandler).Methods("POST")
+	router.HandleFunc("/api/invite-requests/get", getInviteRequestHandler).Methods("GET")
+	router.HandleFunc("/api/invite-requests/update", updateInviteRequestHandler).Methods("PUT")
+	router.HandleFunc("/api/invite-requests/delete", deleteInviteRequestHandler).Methods("DELETE")
 
 	router.HandleFunc("/", indexHandler).Methods("GET")
 
